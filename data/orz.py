08f1bc9a47de3a9ff693aaf5c45fec1375555b08
Kan-Ru Chen <kanru@kanru.info>
2005-07-26 12:56:34 +0000

    default set to no.

--- data/orz.py
+++ data/orz.py
@@ -1,8 +1,14 @@
+from string import *
 from PCManX import *
 from PCManXlib import *
+import eliza
+
+bot = eliza.eliza();
 
 def OnNewIncomingMessage(handle, msg):
-	SendString(handle, "XD")
+	rmsg = split(msg, " ", 2)[2]
+	reply = bot.respond(rmsg)
+	SendString(handle, "" + reply)
 	SendReturn(handle)
 	SendString(handle, "Y")
 	SendReturn(handle)
