face1b08a54f16a48e912525b39956b79550ed6a
Kan-Ru Chen <kanru@kanru.info>
2005-08-25 07:51:07 +0000

    revert change.

--- src/nancy_bot/standalone.cpp
+++ src/nancy_bot/standalone.cpp
@@ -1,22 +1,9 @@
 #include "nancy_bot/api.h"
-#include <stdio.h>     /* standard I/O functions                         */
-#include <unistd.h>    /* standard unix functions, like getpid()         */
-#include <sys/types.h> /* various type definitions, like pid_t           */
-#include <signal.h>    /* signal name macros, and the signal() prototype */
-
-NancyBot *b;
-/* first, here is the signal handler */
-void catch_int(int sig_num)
-{
-	delete b;
-	exit(0);
-}
-
 #ifdef CONSOLE_BOT
 int
 main (int argc, char **argv)
 {
-	signal(SIGINT, catch_int);
+    NancyBot *b;
     if(argv[1] != NULL)
     {
     	b = new NancyBot(argv[1]);
@@ -33,13 +20,9 @@ main (int argc, char **argv)
 	    cin.getline(input,80);
 	    cin.clear();
 	    msg_in = input;
-		if(strstr(input, "reload") != 0) {
-			delete b;
-			b = new NancyBot();
-			continue;
-		}
     }
-	while ((cout << "Nancy: " << b->askNancy (msg_in) << endl));
+    while (strncmp (input, "exit", 4) != 0 &&
+	   (cout << "Nancy: " << b->askNancy (msg_in) << endl));
     // ask_nancy return "PCManX-NancyBot" when initial error ( no config file found )
     delete b;
     return 0;
