8ee4a862c3136ad2f30ff2d2cca33a8ee7f70902
Jim Huang <jserv@0xlab.org>
2008-03-22 18:52:53 +0000

    2008-03-23  Jim Huang  <jserv.tw@gmail.com>
            * src/generalprefpage.cpp (CGeneralPrefPage::CGeneralPrefPage): Fixlet
            to conditional USE_MOUSE.

--- src/generalprefpage.cpp
+++ src/generalprefpage.cpp
@@ -52,9 +52,11 @@ CGeneralPrefPage::CGeneralPrefPage()
 	gtk_widget_show (m_CancelSelAfterCopy);
 	gtk_box_pack_start (GTK_BOX (m_Widget), m_CancelSelAfterCopy, FALSE, FALSE, 0);
 
+#ifdef USE_MOUSE
 	m_MouseSupport = gtk_check_button_new_with_mnemonic (_("Enable Mouse Support"));
 	gtk_widget_show (m_MouseSupport);
 	gtk_box_pack_start (GTK_BOX (m_Widget), m_MouseSupport, FALSE, FALSE, 0);
+#endif
 
 	m_ShowTrayIcon = gtk_check_button_new_with_mnemonic (_("Show System Tray Icon (Docklet) (Take effect after restart)"));
 	gtk_widget_show (m_ShowTrayIcon);
