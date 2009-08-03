aa9fe4d7218adf27d380e5d36a62f291fdaa9ffb
Shih-Yuan Lee (FourDollars) <fourdollars@gmail.com>
2009-08-03 04:12:19 +0000

    Bump to 0.3.9

--- developers/make-new-release.sh
+++ developers/make-new-release.sh
@@ -1,9 +1,11 @@
-#!/bin/bash
+#! /usr/bin/env bash
 
 abort() {
-  echo $*
-  exit 1
+    echo $*
+    exit 1
 }
+
+features=('debug' 'plugin' 'iplookup' 'proxy' 'wget' 'libnotify')
 
 VERSION=
 
@@ -12,33 +14,23 @@ echo "Preparing new release, please stand by..."
 if make dist-bzip2 >/dev/null; then
     VERSION=`grep AC_INIT configure.ac | sed -e 's#AC_INIT(\[pcmanx-gtk2\],\[##g' -e 's#\],\[http://groups.google.com/group/PCManX\])##g'`
 else
-    abort Tarball generation fails.    
+    abort Tarball generation fails.
 fi
 echo
 
-echo "Attempting to build plugin support..."
-tar jxf pcmanx-gtk2-${VERSION}.tar.bz2
-pushd pcmanx-gtk2-${VERSION}
-if ./configure --enable-plugin >/dev/null && make >/dev/null 2>/dev/null; then
-  echo -e "\033[44;37mPassed!\033[m"
-else
-  abort Plugin build fails.
-fi
-popd
-rm -rf pcmanx-gtk2-${VERSION}
-echo
-
-echo "Attempting to build wget support..."
-tar jxf pcmanx-gtk2-${VERSION}.tar.bz2
-pushd pcmanx-gtk2-${VERSION}
-if ./configure --enable-wget >/dev/null && make >/dev/null 2>/dev/null; then
-  echo -e "\033[44;37mPassed!\033[m"
-else
-  abort External wget support build fails.
-fi
-popd
-rm -rf pcmanx-gtk2-${VERSION}
-echo
+for ((i=0; i<${#features[@]}; i++)); do
+    echo "Attempting to build ${features[$i]} support..."
+    tar jxf pcmanx-gtk2-${VERSION}.tar.bz2
+    pushd pcmanx-gtk2-${VERSION}
+    if ./configure --enable-${features[$i]} >/dev/null && make >/dev/null 2>/dev/null; then
+        echo -e "\033[44;37mPassed!\033[m"
+    else
+        abort "feature '${features[$i]}' build fails."
+    fi
+    popd
+    rm -rf pcmanx-gtk2-${VERSION}
+    echo
+done
 
 echo "Tarball pcmanx-gtk2-${VERSION} is ready now!"
 echo
