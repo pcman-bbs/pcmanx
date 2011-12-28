ff242726b451e10f7154bae324b598c87bfc3b52
Shih-Yuan Lee (FourDollars) <fourdollars@gmail.com>
2011-12-28 16:23:27 +0800

    Generate ChangeLog from git commit messages.
    * build/changelog.sh: Used to generate ChangeLog.

---
+++ build/changelog.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+# Convert git log to GNU-style ChangeLog file.
+if test -d ".git"; then
+  git log --date-order --date=short --since="`date -r ChangeLog.old "+%F %T"`" | \
+    sed -e '/^commit.*$/d' | \
+    awk '/^Author/ {sub(/\\$/,""); getline t; print $0 t; next}; 1' | \
+    sed -e 's/^Author: //g' | \
+    sed -e 's/>Date:   \([0-9]*-[0-9]*-[0-9]*\)/>\t\1/g' | \
+    sed -e 's/^\(.*\) \(<.*>\)\t\(.*\)/\3  \1 \2/g' -e 's/^    /\t/g' > ChangeLog
+  exit 0
+else
+  echo "No git repository present."
+  exit 1
+fi
