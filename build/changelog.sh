f02ae4fcaddc804e164076c21defa3ab80ab3b81
Shih-Yuan Lee (FourDollars) <fourdollars@gmail.com>
2011-12-28 21:35:56 +0800

    * build/changelog.sh: Polish the script to generate ChangeLog.

--- build/changelog.sh
+++ build/changelog.sh
@@ -1,14 +1,15 @@
 #!/bin/sh
 # Convert git log to GNU-style ChangeLog file.
-if test -d ".git"; then
-  git log --date-order --date=short --since="Wed Dec 28 16:23:27 2011 +0800" | \
-    sed -e '/^commit.*$/d' | \
-    awk '/^Author/ {sub(/\\$/,""); getline t; print $0 t; next}; 1' | \
-    sed -e 's/^Author: //g' | \
-    sed -e 's/>Date:   \([0-9]*-[0-9]*-[0-9]*\)/>\t\1/g' | \
-    sed -e 's/^\(.*\) \(<.*>\)\t\(.*\)/\3  \1 \2/g' -e 's/^    /\t/g' > ChangeLog
-  exit 0
-else
-  echo "No git repository present."
-  exit 1
+
+if [ -d .git ]; then
+    echo
+elif [ -d ../.git ]; then
+    cd ..
 fi
+
+git log --date-order --date=short --since="Wed Dec 28 16:23:27 2011 +0800" | \
+  sed -e '/^commit.*$/d' | \
+  awk '/^Author/ {sub(/\\$/,""); getline t; print $0 t; next}; 1' | \
+  sed -e 's/^Author: //g' | \
+  sed -e 's/>Date:   \([0-9]*-[0-9]*-[0-9]*\)/>\t\1/g' | \
+  sed -e 's/^\(.*\) \(<.*>\)\t\(.*\)/\3  \1 \2/g' -e 's/^    /\t/g'
