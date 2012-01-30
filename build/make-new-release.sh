343aa742cb17071ed86c6179a6c5e4e3a8b52556
Shih-Yuan Lee (FourDollars) <fourdollars@gmail.com>
2012-01-30 14:30:39 +0800

    * build/make-new-release.sh: Modify for xz compressing format.

--- build/make-new-release.sh
+++ build/make-new-release.sh
@@ -14,7 +14,7 @@ VERSION=
 
 clear
 echo "Preparing new release, please stand by..."
-if make dist-bzip2 >/dev/null; then
+if make dist >/dev/null; then
     VERSION="$(../configure --version | head -n1 | awk '{ print $3 }')"
 else
     abort Tarball generation fails.
@@ -23,7 +23,7 @@ echo
 
 for ((i=0; i<${#features[@]}; i++)); do
     echo "Attempting to build ${features[$i]} support..."
-    tar xjf pcmanx-gtk2-${VERSION}.tar.bz2
+    tar xJf pcmanx-gtk2-${VERSION}.tar.xz
     pushd pcmanx-gtk2-${VERSION}
     if ./configure --enable-${features[$i]} > ../build-${features[$i]}.log && make >> ../build-${features[$i]}.log 2>&1; then
         echo -e "\033[44;37mPassed!\033[m"
