5e8dffd29008e8805c394c61cd8585ac5118918f
noguchi167 <noguchi@noguchi-Parallels-Virtual-Platform.(none)>
2015-05-10 13:41:28 +0800

    fix issue #33 and #39,
    Reconnect without opening a new tab.
    If title/url includ string "telnet://", remove it.

--- src/mainframe.cpp
+++ src/mainframe.cpp
@@ -318,6 +318,14 @@ CTelnetCon* CMainFrame::NewCon(string title, string url, CSite* site )
 	m_pView->m_CharPaddingX = AppConfig.CharPaddingX;
 	m_pView->m_CharPaddingY = AppConfig.CharPaddingY;
 
+	// issue #39
+        if ( !title.compare( 0, strlen("telnet://"), "telnet://" ) ) {
+                title = title.substr( strlen("telnet://") );
+        }
+        if ( !url.compare( 0, strlen("telnet://"), "telnet://" ) ) {
+                url = url.substr( strlen("telnet://") );
+        }
+
 	pCon->m_Site.m_Name = title;
 	pCon->m_Site.m_URL = url;
 	pCon->m_Encoding = pCon->m_Site.m_Encoding;
@@ -1724,10 +1732,9 @@ void CMainFrame::OnReconnect(GtkMenuItem* mitem UNUSED, CMainFrame* _this)
 	CTelnetCon* con = _this->GetCurCon();
 	if( !con )
 		return;
-	if( con->IsClosed() )
-		con->Reconnect();
-	else
-		_this->NewCon( con->m_Site.m_Name, con->m_Site.m_URL, &con->m_Site);
+
+	// issue #33
+	con->Reconnect();
 }
 
 void CMainFrame::FlashWindow( bool flash )
