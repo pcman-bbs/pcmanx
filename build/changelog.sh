a5f7ecc7e508f8767e2a6f52a10f2d134169201e
Jim Huang <jserv.tw@gmail.com>
2015-06-17 17:18:14 +0800

    changelog: exclude GIT merge info

--- build/changelog.sh
+++ build/changelog.sh
@@ -12,4 +12,5 @@ git log --date-order --date=short --since="Wed Dec 28 16:23:27 2011 +0800" | \
   awk '/^Author/ {sub(/\\$/,""); getline t; print $0 t; next}; 1' | \
   sed -e 's/^Author: //g' | \
   sed -e 's/>Date:   \([0-9]*-[0-9]*-[0-9]*\)/>\t\1/g' | \
-  sed -e 's/^\(.*\) \(<.*>\)\t\(.*\)/\3  \1 \2/g' -e 's/^    /\t/g'
+  sed -e 's/^\(.*\) \(<.*>\)\t\(.*\)/\3  \1 \2/g' -e 's/^    /\t/g' |
+  sed -e '/^Merge: /d'
